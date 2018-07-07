05dd190710c44e9721735273a17aad1b6571a68d
Fraser Sprott <frasers@hotmail.co.uk>
2018-07-07 17:29:54 +0100

    added a changable delay using asynchronous functions and formatted the output.

---
+++ dotnet-basics/dotnet-basics/TelePrompterConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Math;
+
+internal class TelePrompterConfig
+{
+    private object lockHandle = new object();
+    public int DelayInMilliSeconds { get; private set; } = 200;
+
+    public void UpdateDelay(int increment)
+    {
+        var newDelay = Min(DelayInMilliSeconds + increment, 1000);
+        newDelay = Max(newDelay, 20);
+        lock (lockHandle)
+        {
+            DelayInMilliSeconds = newDelay;
+        }
+    }
+
+    public bool Done { get; private set; }
+
+    public void SetDone()
+    {
+        Done = true;
+    }
+}
